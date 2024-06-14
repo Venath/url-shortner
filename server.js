9dc56ffe1b7a07f4d002fe3101415e12ba57d77f
Venath <138109653+Venath@users.noreply.github.com>
2024-06-14 18:28:40 +0530

    initial

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/urlShortener', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.error("Failed to connect to MongoDB", err);
+});
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+  try {
+    const shortUrls = await ShortUrl.find();
+    res.render('index', { shortUrls: shortUrls });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/shortUrls', async (req, res) => {
+  try {
+    await ShortUrl.create({ full: req.body.fullUrl });
+    console.log(req.body.fullUrl)
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/:shortUrl', async (req, res) => {
+  try {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+    if (shortUrl == null) return res.sendStatus(404);
+
+    shortUrl.clicks++;
+    await shortUrl.save();
+
+    res.redirect(shortUrl.full);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Server started on port 5000');
+});
